d11cc536074d91491d4560058da1074b2df2a819
yu1git <mitateyuu@gmail.com>
2023-04-14 18:52:20 +0900

    0~1章

---
+++ 1/1-15/challenge_variable.php
@@ -0,0 +1,15 @@
+<?php
+    $hp = 200;
+    $mp = 80;
+    $sp = 50;
+ 
+    print '現在のhpは';
+    print $hp;
+    print 'です。';
+ 
+    print '現在のmpは';
+    print $mp;
+    print 'です。';
+
+    print '現在のspは'.$sp.'です。';
+?>
